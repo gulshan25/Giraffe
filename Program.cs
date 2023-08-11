878ca5ad75f0dc4128f8da88828f8009e0abcf26
gulshan25 <g.r.tanny@gmail.com>
2023-08-11 22:08:27 +0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("//");
-Console.WriteLine("//");
-Console.WriteLine("//");
-Console.WriteLine("//");
-Console.WriteLine("//");
+Console.WriteLine("/___/");
+Console.WriteLine("/___/");
+Console.WriteLine("/___/");
+Console.WriteLine("/___/");
+Console.WriteLine("/___/");
 
 Console.ReadLine();
