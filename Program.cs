6ccebc2af650a64681ad2a2b45daf54575bd11e7
gulshan25 <g.r.tanny@gmail.com>
2023-08-12 11:54:40 +0600

    first commit

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine("/___/");
-Console.WriteLine("/___/");
-Console.WriteLine("/___/");
-Console.WriteLine("/___/");
-Console.WriteLine("/___/");
+
+// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("/___/");
+// Console.WriteLine("/___/");
+// Console.WriteLine("/___/");
+// Console.WriteLine("/___/");
+// Console.WriteLine("/___/");
+
+
+string characterName = "Tom";
+int characterAge;
+characterAge = 35;
+
+Console.WriteLine("There once was a man named " + characterName);
+Console.WriteLine("He was "+ characterAge +" years old");
+
+characterName = "fed";
+characterAge = 0;
+Console.WriteLine("He really liked the name " + characterName);
+Console.WriteLine("But didn't like being " + characterAge);
 
 Console.ReadLine();
+
